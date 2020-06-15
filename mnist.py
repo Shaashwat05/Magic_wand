06d0b3ce3567c72ff8e86b027fb64d9420f0f664
Shaashwat <shaas2000@gmail.com>
2020-06-15 20:07:49 +0530

    classifier added

--- mnist.py
+++ mnist.py
@@ -7,17 +7,20 @@ config=tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth=True
 session=tf.compat.v1.InteractiveSession(config=config)
 
-model = load_model("cnn.h5")
+def predict(im):
 
-im = cv2.imread("frame1.jpg")
+    model = load_model("cnn.h5")
 
-im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    im = cv2.resize(im, (28,28))
+    im = np.reshape(im, (1,28*28))
 
-im = cv2.resize(im, (28,28))
+    prediction = model.predict(im)
 
-im = np.reshape(im, (1,28*28))
+    num = np.argmax(prediction)
 
+    print(prediction)
+    prob = prediction[0][num]
 
-img = np.argmax(model.predict(im))
+    return num,prob
 
-print(img)
--- wand.py
+++ wand.py
@@ -1,8 +1,13 @@
 import cv2
 import numpy as np
 import time
+from mnist import predict
 
-x, y, k = 100, 100, -1
+x, y, k = 200, 200, -1
+
+kernel1=np.asarray([[1,0,-1],[1,0,-1],[1,0,-1]])
+kernel2=np.asarray([[1,1,1],[0,0,0],[-1,-1,-1]])
+
 
 cap = cv2.VideoCapture(0)
 
@@ -12,19 +17,32 @@ old_pts = np.array([[x, y]], dtype=np.float32).reshape(-1, 1, 2)
 
 ret, frame = cap.read()
 gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#edges=np.sqrt(convolution(np.float32(gray_frame/255),kernel1)**2+convolution(np.float32(gray_frame/255),kernel2)**2)
+#ret,thresh2 = cv2.threshold(edges*255,80,255,cv2.THRESH_BINARY)
 
 mask = np.ones_like(frame)
 
+time.sleep(3)
 print(mask)
+
+lower_red = np.array([30,150,50])
+upper_red = np.array([255,255,180])
+
+
+def command(num):
+    if(num == 1):
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, lower_red, upper_red)
+ 
+
+count = 0
 
 while True:
     check, new_frame = cap.read()
     new_frame = cv2.flip(new_frame, 1)
     new_gray = cv2.cvtColor(new_frame, cv2.COLOR_BGR2GRAY)
-    new_gray = cv2.blur(new_gray,(7,7))
-    new_gray = cv2.blur(new_gray,(7,7))
-    new_gray = cv2.blur(new_gray,(7,7))
-    new_gray = cv2.blur(new_gray,(7,7))
+    #edges=np.sqrt(convolution(np.float32(new_gray/255),kernel1)**2+convolution(np.float32(new_gray/255),kernel2)**2)
+    #ret,thresh1 = cv2.threshold(edges*255,80,255,cv2.THRESH_BINARY)
     cv2.imshow("gray", new_gray)
     new_pts, status, err = cv2.calcOpticalFlowPyrLK(gray_frame,
                                                     new_gray,
@@ -40,14 +58,27 @@ while True:
             mask = cv2.line(mask, (a, b), (x, y), (255, 255, 255), 6)
 
         cv2.circle(new_frame, (x, y), 6, (0, 255, 0), -1)
+    
+    num, prob = predict(mask)
+    if(prob > 0.7):
+        filter = num
 
     new_frame = cv2.addWeighted(mask, 0.3, new_frame, 0.7, 0)
-    cv2.imshow("OutPut Window", new_frame)
+    new_frame = cv2.rectangle(new_frame, (0,0), (50, 50), (127, 255, 127), -1) 
+    if(filter == 1):
+        hsv = cv2.cvtColor(new_frame, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, lower_red, upper_red)
+        cv2.imshow("OutPut Window", mask)
+    elif(filter == 2):
+
+    
+
     cv2.imshow("Result Window", mask)
 
     cv2.imwrite("frame1.jpg", mask)
 
     gray_frame = new_gray.copy()
+
     old_pts = new_pts.reshape(-1, 1, 2)
     key = cv2.waitKey(1)
     if key == 27:
